7ba3844810ae08486513c08410744ce4249e86aa
Austin Liu <austinhliu@gmail.com>
2023-01-12 15:49:16 -0800

    Added test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+def main():
+  x = "ECE_180_DA_DB"
+  if x == "ECE_180_DA_DB":
+      print("You are living in 2017")
+  else:
+      # this is a comment
+      x = x + " - Best class ever"
+      print(x)
+
+if __name__ == "__main__":
+  main()
